069581d8fc879a46a0e2308a47630d3789e806b0
hmaria <mh6417a@gmail.com>
2022-08-02 23:32:19 -0400

    added / path

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 app.use(cors());
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
 
 app.listen(3000, () => console.log('listening on port 3000'));
